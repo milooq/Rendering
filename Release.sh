6be0f75663b8295ded6bd8b99b833a7d8aa4cb1f
milooq <animash582@gmail.com>
2020-12-19 16:55:20 +0300

    del

--- Release.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-mkdir -p bin
-mkdir -p obj
-mkdir -p obj/Release
-make $1 --directory=./obj/Release --makefile=../../Makefile build_flags="-O2 -fomit-frame-pointer" program_name=../../bin/application link_flags="-s -pipe -std=c++17"
